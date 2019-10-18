c43630854c8a23f11908bd44944b6e0d9d840816
Chris Carpentier <coredump@gmail.com>
2019-10-18 10:06:00 -0400

    Initial Import

---
+++ java/src/main/java/io/christop/Application.java
@@ -0,0 +1,10 @@
+package io.christop;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
